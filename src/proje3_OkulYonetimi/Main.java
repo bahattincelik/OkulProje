c7e29eb92b79c260acb9ed3e57b51ea061069148
Bahattin Celik <75189249+bahattincelik@users.noreply.github.com>
2021-04-15 23:08:53 +0300

    okul yonetimi-2

---
+++ src/proje3_OkulYonetimi/Main.java
@@ -0,0 +1,12 @@
+package proje3_OkulYonetimi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Islemler islem = new Islemler();
+
+        islem.anaMenu();
+    }
+
+}
---
+++ src/proje3_OkulYonetimi/Ogretmen.java
@@ -0,0 +1,30 @@
+package proje3_OkulYonetimi;
+
+public class Ogretmen extends Kisi {
+
+    private String bolum;
+    private String sicilNo;
+
+
+    public Ogretmen(String adSoyadi, String kimlikNo, int yas, String bolum, String sicilNo) {
+        super(adSoyadi, kimlikNo, yas);
+        this.bolum = bolum;
+        this.sicilNo = sicilNo;
+    }
+
+    public String getBolum() {
+        return bolum;
+    }
+
+    public void setBolum(String bolum) {
+        this.bolum = bolum;
+    }
+
+    public String getSicilNo() {
+        return sicilNo;
+    }
+
+    public void setSicilNo(String sicilNo) {
+        this.sicilNo = sicilNo;
+    }
+}
